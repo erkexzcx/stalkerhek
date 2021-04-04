179ca4d7eddf4356abcdafc5eda07fe0dad0da23
Erikas Rudinskas <erikmnkl@gmail.com>
2021-04-04 23:55:15 +0300

    let's do what we always do - try to fix "rewrite" option

--- proxy/utils.go
+++ proxy/utils.go
@@ -34,7 +34,7 @@ func addHeaders(from, to http.Header) {
 }
 
 func generateNewChannelLink(link, id, ch_id string) string {
-	return `{"js":{"id":"` + ch_id + `","cmd":"` + specialLinkEscape(link) + `","streamer_id":0,"link_id":` + id + `,"load":0,"error":""},"text":"array(6) {\n  [\"id\"]=>\n  string(4) \"` + ch_id + `\"\n  [\"cmd\"]=>\n  string(99) \"` + specialLinkEscape(link) + `\"\n  [\"streamer_id\"]=>\n  int(0)\n  [\"link_id\"]=>\n  int(` + id + `)\n  [\"load\"]=>\n  int(0)\n  [\"error\"]=>\n  string(0) \"\"\n}\ngenerated in: 0.01s; query counter: 8; cache hits: 0; cache miss: 0; php errors: 0; sql errors: 0;"}`
+	return `{"js":{"id":"` + id + `","cmd":"` + specialLinkEscape(link) + `","streamer_id":0,"link_id":` + ch_id + `,"load":0,"error":""},"text":"array(6) {\n  [\"id\"]=>\n  string(4) \"` + id + `\"\n  [\"cmd\"]=>\n  string(99) \"` + specialLinkEscape(link) + `\"\n  [\"streamer_id\"]=>\n  int(0)\n  [\"link_id\"]=>\n  int(` + ch_id + `)\n  [\"load\"]=>\n  int(0)\n  [\"error\"]=>\n  string(0) \"\"\n}\ngenerated in: 0.01s; query counter: 8; cache hits: 0; cache miss: 0; php errors: 0; sql errors: 0;"}`
 }
 
 func specialLinkEscape(i string) string {
